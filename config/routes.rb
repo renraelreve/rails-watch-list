440f181df8c105fc678478444a3a8a294c81a1c0
teckguan ng <renraelreve@gmail.com>
2023-08-19 21:16:20 +0800

    backend done now do front end

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'lists/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :lists do
+    resources :bookmarks, only: [:new, :create, :index]
+  end
+
+  resources :bookmarks, only: [:destroy]
 end
